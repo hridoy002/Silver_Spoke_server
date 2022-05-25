da55166f1947af1c29d71705d763308ea6cf4c7d
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-26 03:43:44 +0600

    load single data

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
@@ -13,19 +13,28 @@ app.use(express.json())
 
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lwvxc.mongodb.net/?retryWrites=true&w=majority`;
-console.log(uri)
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 async function  run(){
     try{
-        await client.connect();
-        const toolsCollection = client.db('silver_spoke').collecton('tools');
+        await client.connect()
+        const toolsCollection = client.db("silver-spoke").collection("tools");
 
-        app.get('/tools', async(req,res) => {
-            const query = {};
+        // get all tools 
+        app.get("/tools", async(req,res)=>{
+            const query ={};
             const cursor = toolsCollection.find(query);
             const tools = await cursor.toArray();
             res.send(tools);
+        })
+
+        // get tool by id
+        // get individual product id 
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const item = await toolsCollection.findOne(query);
+            res.send(item);
         })
     }
     finally{
@@ -36,11 +45,11 @@ async function  run(){
 run().catch(console.dir);
 
 app.get('/', (req, res) => {
-  res.send('Silver Spoke server')
+  res.send('Silver Spoke server,')
 })
 
 app.listen(port, () => {
-  console.log(`Silver Spoke server conditin good ${port}`)
+  console.log(`Silver Spoke server condition good ${port}`)
 })
 
 
