531f1ed88616e07cc17c473cf2d570f49967941c
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-25 23:52:48 +0600

    mongodb connect

--- index.js
+++ index.js
@@ -1,13 +1,39 @@
+const { MongoClient, ServerApiVersion } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
-require =('dotenv').config();
+require('dotenv').config();
 
 
 app.use(cors());
 app.use(express.json())
 
+
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lwvxc.mongodb.net/?retryWrites=true&w=majority`;
+console.log(uri)
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+async function  run(){
+    try{
+        await client.connect();
+        const toolsCollection = client.db('silver_spoke').collecton('tools');
+
+        app.get('/tools', async(req,res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query);
+            const tools = await cursor.toArray();
+            res.send(tools);
+        })
+    }
+    finally{
+
+    }
+}
+
+run().catch(console.dir);
 
 app.get('/', (req, res) => {
   res.send('Silver Spoke server')
