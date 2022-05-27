9958f2e829c67a5c77bfbf5762443d800ef8bc96
hridoy002 <hridoyalhasan2@gmail.com>
2022-05-27 15:10:06 +0600

    order collection

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-const { MongoClient, ServerApiVersion,ObjectId } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const express = require('express')
 const app = express()
 const port = process.env.PORT || 5000;
 const cors = require('cors');
 require('dotenv').config();
+const jwt = require('jsonwebtoken');
 
 
 app.use(cors());
@@ -15,31 +16,55 @@ app.use(express.json())
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.lwvxc.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
-async function  run(){
-    try{
-        await client.connect()
-        const toolsCollection = client.db("silver-spoke").collection("tools");
+async function run() {
+  try {
+    await client.connect()
+    const toolsCollection = client.db("silver-spoke").collection("tools");
+    const usersCollection = client.db("silver-spoke").collection("users");
+    const orderCollection = client.db("silver-spoke").collection("order")
 
-        // get all tools 
-        app.get("/tools", async(req,res)=>{
-            const query ={};
-            const cursor = toolsCollection.find(query);
-            const tools = await cursor.toArray();
-            res.send(tools);
-        })
+    // get all tools 
+    app.get("/tools", async (req, res) => {
+      const query = {};
+      const cursor = toolsCollection.find(query);
+      const tools = await cursor.toArray();
+      res.send(tools);
+    })
 
-        // get tool by id
-        // get individual product id 
-        app.get('/tools/:id', async (req, res) => {
-            const id = req.params.id;
-            const query = { _id: ObjectId(id) };
-            const item = await toolsCollection.findOne(query);
-            res.send(item);
-        })
-    }
-    finally{
+    // get tool by id
+    // get individual product id 
+    app.get('/tools/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: ObjectId(id) };
+      const item = await toolsCollection.findOne(query);
+      res.send(item);
+    })
 
-    }
+    // user creation 
+    app.put('/user/:email', async (req, res) => {
+      const email = req.params.email;
+      const user = req.body;
+      const filter = { email: email };
+
+      const options = { upsert: true };
+      const updateDoc = {
+        $set: user,
+      };
+      const result = await usersCollection.updateOne(filter, updateDoc, options);
+      const token = jwt.sign({ email: email }, process.env.SECRET_TOKEN, { expiresIn: '5h' })
+      res.send({ result, token });
+    })
+
+    // order collection
+    app.post('/order', async (req, res) => {
+      const data = req.body;
+      const result = await orderCollection.insertOne(data);
+      res.send(result);
+    })
+  }
+  finally {
+
+  }
 }
 
 run().catch(console.dir);
